019cae8e04d63af6dfaffbbd891150a5a8bb465f
ComradeVijay <comradevijay93@gmail.com>
2026-06-08 10:49:22 +0530

    solved 3512. Minimum Operations to Make Array Sum Divisible by K

---
+++ LeetCodeProblems/MinimumOperations.java
@@ -0,0 +1,20 @@
+//! 3512. Minimum Operations to Make Array Sum Divisible by K
+//? Input: nums = [3,9,7], k = 5
+//? Output: 4
+
+class MinimumOperations{
+    static int minOperations(int arr[], int k){
+        
+        int total = 0;
+        for(int i = 0; i< arr.length; i++){
+            total += arr[i];
+        }
+        return total % k;
+    }
+    public static void main(String args[]){
+        int arr[] = {3,9,7};
+        int k = 5;
+        System.out.println(minOperations(arr, k));
+
+    }
+}
